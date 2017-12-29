6ef3cfac32edd7ce7617b6bdaa2b0fa0f1ea7511
Ubuntu <odk410@naver.com>
2017-12-29 02:14:16 +0000

    lotto

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -12,11 +12,30 @@ class KakaoController < ApplicationController
     # message parameter로 user_key, type, content가 온다.
     user_message = params[:content]
 
-    return_message = {
-      :message => {
-        :text => user_message
+    # 로또
+    lotto = (1..45).sample(6).sort
+    lotto = lotto.to_s
+
+    # 메뉴
+    menu = ["한식", "중식", "일식", "패스트 푸드"]
+    choice = menu.sample
+
+    # 로또와 메뉴 외 다른 명령어가 들어 왔을 때
+    # => ㅠㅠ 알 수 없는 명령어 입니다.
+
+    if params[:text] == "로또"
+      return_message = {
+        :message =>{
+          :text => lotto
+        }
       }
-    }
+
+    else
+      return_message = {
+        :message => {
+          :text => "ㅠㅠ 알 수 없는 명령어 입니다."
+        }
+      }
 
     render json: return_message
   end
