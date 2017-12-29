25bb26dd54f69dff73643320354f62c974659c52
Ubuntu <odk410@naver.com>
2017-12-29 04:07:03 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   post '/message' => 'kakao#message'
 
+  get '/message' => 'kakao#message'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
