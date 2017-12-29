1ad5ec7d338fe17370922b5c09d592f1a0b51925
Ubuntu <odk410@naver.com>
2017-12-29 06:29:44 +0000

    module (movie, cat)

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -1,3 +1,5 @@
+require 'parser' #helper에 등록한 것을 자동으로 찾는다.
+
 class KakaoController < ApplicationController
   def keyboard
     home_keyboard = {
@@ -26,34 +28,47 @@ class KakaoController < ApplicationController
 
     elsif user_message == "고양이"
       #고양이 사진 보여주기
-      return_text = "난 고양이 없다..."
+      # return_text = "난 고양이 없다..."
+      # image = true
+      # url = "http://thecatapi.com/api/images/get?format=xml&type=jpg"
+      # cat_xml = RestClient.get(url)
+      # doc = Nokogiri::XML(cat_xml)
+      # img_url = doc.xpath("//url").text
+
       image = true
-      url = "http://thecatapi.com/api/images/get?format=xml&type=jpg"
-      cat_xml = RestClient.get(url)
-      doc = Nokogiri::XML(cat_xml)
-      cat_url = doc.xpath("//url").text
+      animal = Parser::Animal.new
+      puts animal.methods
+      cat_info = animal.cat
+      return_text = cat_info[0]
+      img_url = cat_info[1]
 
     # 네이버 영화 제목, 이미지, 평점 가져오기
     elsif user_message == "영화"
-      image = true
-  		url = "http://movie.naver.com/movie/running/current.nhn?view=list&tab=normal&order=reserve"
-  		movie_html = RestClient.get(url)
-  		doc = Nokogiri::HTML(movie_html)
+      #image = true
+  		# url = "http://movie.naver.com/movie/running/current.nhn?view=list&tab=normal&order=reserve"
+  		# movie_html = RestClient.get(url)
+  		# doc = Nokogiri::HTML(movie_html)
+      #
+  		# movie_title = Array.new
+  		# movie_info = Hash.new
+  		# doc.css("ul.lst_detail_t1 dt a").each do |title|
+  		# 	movie_title << title.text
+  		# end
+		  # doc.css("ul.lst_detail_t1 li").each do |movie|
+  		# 	movie_info[movie.css("dl dt.tit a").text] = {
+  		# 		:url => movie.css("div.thumb img").attribute('src').to_s,
+  		# 		:star => movie.css("dl.info_star span.num").text
+  		# }
+  		# end
+  		# sample_movie = movie_title.sample
+  		# return_text = sample_movie + " " + movie_info[sample_movie][:star]
+  		# cat_url = movie_info[sample_movie][:url]
 
-  		movie_title = Array.new
-  		movie_info = Hash.new
-  		doc.css("ul.lst_detail_t1 dt a").each do |title|
-  			movie_title << title.text
-  		end
-		  doc.css("ul.lst_detail_t1 li").each do |movie|
-  			movie_info[movie.css("dl dt.tit a").text] = {
-  				:url => movie.css("div.thumb img").attribute('src').to_s,
-  				:star => movie.css("dl.info_star span.num").text
-  		}
-  		end
-  		sample_movie = movie_title.sample
-  		return_text = sample_movie + " " + movie_info[sample_movie][:star]
-  		cat_url = movie_info[sample_movie][:url]
+      image = true
+      naver_movie = Parser::Movie.new # Parser에 있는 Movie라는 클래스를 만든다.
+      naver_movie_info = naver_movie.naver
+      return_text = naver_movie_info[0]
+      img_url = naver_movie_info[1]
 
     else
       return_text = "ㅠㅠ 알 수 없는 명령어 입니다. [로또], [메뉴], [고양이] 중 하나를 입력해 주세요~!"
@@ -73,7 +88,7 @@ class KakaoController < ApplicationController
       :message => {
         :text => return_text,
         :photo => {
-          :url => cat_url,
+          :url => img_url,
           :width => 640,
           :height => 480
         }
