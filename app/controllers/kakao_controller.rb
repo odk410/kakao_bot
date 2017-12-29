e13df390d859e178050378b902d19c4c3c217aa7
Ubuntu <odk410@naver.com>
2017-12-29 01:28:04 +0000

    keyboard + message

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -8,5 +8,16 @@ class KakaoController < ApplicationController
   end
 
   def message
+    #사용자가 보내준 텍스트를 그대로 다시 말하기
+    # message parameter로 user_key, type, content가 온다.
+    user_message = params[:content]
+
+    return_message = {
+      :message => {
+        :text => user_message
+      }
+    }
+
+    render json: return_message
   end
 end
